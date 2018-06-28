a39f9753a09d5f6234831671bbdc21c25d4418da
Lukas van Driel <lukas@q42.nl>
2018-06-28 20:57:59 +0200

    martin z'n merge conflicts opgelost

--- gruntfile.js
+++ gruntfile.js
@@ -3,8 +3,41 @@ module.exports = function(grunt) {
   //grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-uglify-es');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
+  var files = [
+    'readyjs/style.js',
+    'readyjs/game.js',
+    'readyjs/utils.js',
+    'readyjs/$.js',
+    'readyjs/keyCodes.js',
+    'readyjs/Keys.js',
+    'readyjs/ready.js',
+    'readyjs/spriteList.js',
+    'readyjs/sprite.js',
+    'readyjs/screen.js',
+    'readyjs/mouse.js',
+    'readyjs/level.js',
+    //'readyjs/controls.js',
+    //'readyjs/viewportmaster.js',
+    //'readyjs/inspector.js',
+    'readyjs/sound.js',
+    'readyjs/storage.js',
+    //'readyjs/log.js',
+    'readyjs/spriteSheet.js'
+  ];
 
   grunt.initConfig({
+
+    concat: {
+      options: {
+        separator: '\n\n// NEXT FILE\n\n',
+      },
+      dist: {
+        src: files,
+        dest: 'ready.dev.js',
+      },
+    },
 
 		uglify : {
 
@@ -14,27 +47,7 @@ module.exports = function(grunt) {
 		  	},
 				files: [
 					{
-						src: [
-              'readyjs/style.js',
-              'readyjs/game.js',
-              'readyjs/utils.js',
-              'readyjs/$.js',
-              'readyjs/keyCodes.js',
-              'readyjs/Keys.js',
-              'readyjs/ready.js',
-              'readyjs/spriteList.js',
-              'readyjs/sprite.js',
-              'readyjs/screen.js',
-              'readyjs/mouse.js',
-              'readyjs/level.js',
-              //'readyjs/controls.js',
-              //'readyjs/viewportmaster.js',
-              //'readyjs/inspector.js',
-              'readyjs/sound.js',
-              'readyjs/storage.js',
-              //'readyjs/log.js',
-              'readyjs/spriteSheet.js'
-						],
+						src: files,
 						dest: 'ready.js'
 					}
 				]
@@ -51,5 +64,5 @@ module.exports = function(grunt) {
   });
 
   grunt.registerTask('min', ['uglify:min']);
-  grunt.registerTask('default', ['min']);
+  grunt.registerTask('default', ['min', 'concat']);
 };
